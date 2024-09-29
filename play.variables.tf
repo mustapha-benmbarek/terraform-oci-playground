d5264f49248875639598ba42452022fc1257b8eb
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2024-09-29 18:27:33 +0400

    Initial commit

--- play.variables.tf
+++ play.variables.tf
@@ -2,10 +2,12 @@
 /*Filepath loading...*/
 locals {
   path = {
-    identity = "${path.module}/parameters/identity/"
-    network  = "${path.module}/parameters/network/"
-    security = "${path.module}/parameters/security/"
-    storage  = "${path.module}/parameters/storage/"
-    database = "${path.module}/parameters/database/"
+    identity  = "${path.module}/parameters/identity/"
+    network   = "${path.module}/parameters/network/"
+    security  = "${path.module}/parameters/security/"
+    storage   = "${path.module}/parameters/storage/"
+    database  = "${path.module}/parameters/database/"
+    analytics = "${path.module}/parameters/analytics/"
+    developer = "${path.module}/parameters/developer/"
   }
 }
