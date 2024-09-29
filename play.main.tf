75ab5df5b56b7214caee53aa667df9bd561701a5
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2024-09-29 18:50:26 +0400

    Initial commit

--- play.main.tf
+++ play.main.tf
@@ -1,4 +1,4 @@
-/*Networking Stack..*/
+/*Networking Stack..
 variable "TENANCY_OCID" {}
 
 module "oci-network" {
@@ -19,3 +19,30 @@ module "oci-network" {
   //json-cluster-placement-groups     = "${local.path.network}/cluster-placement-groups.json"
   //json-customer-premises-equipments = "${local.path.network}/customer-premises-equipments.json"
 }
+
+/*
+module "oci-identity" {
+  source            = "../terraform-oci-core-identity"
+  json-compartments = "${local.path.identity}/compartments.json"
+}*/
+
+/*
+module "oci-network" {
+  source                = "../terraform-oci-core-network"
+  TENANCY_OCID          = var.TENANCY_OCID
+  json-vcns             = "${local.path.network}/vcns.json"
+  json-vcn-dhcp-options = "${local.path.network}/vcn-dhcp-options.json"
+}*/
+
+module "oci-developer" {
+  source             = "../terraform-oci-core-developer"
+  json-topics        = "${local.path.developer}/topics.json"
+  json-subscriptions = "${local.path.developer}/subscriptions.json"
+}
+
+/*
+module "oci-analytics" {
+  source            = "../terraform-oci-core-analytics"
+  json-stream-pools = "${local.path.analytics}/stream-pools.json"
+  json-streams      = "${local.path.analytics}/streams.json"
+}*/
