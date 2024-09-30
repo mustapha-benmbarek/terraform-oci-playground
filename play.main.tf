dcb819ad056ed83a8cf7a118e9fa50e53f548e64
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2024-09-30 17:06:35 +0400

    Initial commit

--- play.main.tf
+++ play.main.tf
@@ -1,48 +1,25 @@
-/*Networking Stack..
-variable "TENANCY_OCID" {}
-
-module "oci-network" {
-  source = "../terraform-oci-core-network"
-
-  TENANCY_OCID = var.TENANCY_OCID
-
-  //identity - to be changed dlater
-  //json-compartments = "${local.path.identity}/compartments.json"
-  json-vcns = "${local.path.network}/vcns.json"
-  //json-vcn-subnets           = "${local.path.network}/vcn-subnets.json"
-  //json-vcn-internet-gateways = "${local.path.network}/vcn-internet-gateways.json"
-  //json-vcn-nat-gateways      = "${local.path.network}/vcn-nat-gateways.json"
-  //json-vcn-service-gateways = "${local.path.network}/vcn-service-gateways.json"
-  json-vcn-dhcp-options = "${local.path.network}/vcn-dhcp-options.json"
-  //json-vcn-route-tables      = "${local.path.network}/vcn-route-tables.json"
-  //json-vcn-security-lists           = "${local.path.network}/vcn-security-lists.json"
-  //json-cluster-placement-groups     = "${local.path.network}/cluster-placement-groups.json"
-  //json-customer-premises-equipments = "${local.path.network}/customer-premises-equipments.json"
-}
-
-/*
+/*Identity Stack..*/
 module "oci-identity" {
   source            = "../terraform-oci-core-identity"
+  data-tenancy-id   = var.tenancy_ocid
   json-compartments = "${local.path.identity}/compartments.json"
-}*/
+}
 
-/*
-module "oci-network" {
-  source                = "../terraform-oci-core-network"
-  TENANCY_OCID          = var.TENANCY_OCID
-  json-vcns             = "${local.path.network}/vcns.json"
-  json-vcn-dhcp-options = "${local.path.network}/vcn-dhcp-options.json"
-}*/
+/*Networking Stack..*/
+/*Security Stack..*/
 
+/*Developer Services Stack..*/
 module "oci-developer" {
   source             = "../terraform-oci-core-developer"
+  data-compartments  = module.oci-identity.compartments
   json-topics        = "${local.path.developer}/topics.json"
   json-subscriptions = "${local.path.developer}/subscriptions.json"
 }
 
-/*
+/*Analytics and AI Stack..*/
 module "oci-analytics" {
   source            = "../terraform-oci-core-analytics"
+  data-compartments = module.oci-identity.compartments
   json-stream-pools = "${local.path.analytics}/stream-pools.json"
   json-streams      = "${local.path.analytics}/streams.json"
-}*/
+}
--- play.variables.tf
+++ play.variables.tf
@@ -1,4 +1,10 @@
 /*Provider variables*/
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+
 /*Filepath loading...*/
 locals {
   path = {
